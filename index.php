8724c6772cde27c9d0af658b805762e2996e2936
moonye@live.fr <moonye@live.fr>
2024-12-03 15:06:09 +0100

    Exo fini

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-$name = "Elodie";
+include_once './algo.php';
 ?>
 
 <!DOCTYPE html>
@@ -10,8 +10,45 @@ $name = "Elodie";
     <title>Document</title>
 </head>
 <body>
+  
     <?php
+// exo 1
+echo $name;
+  
+  
+// exo 2
+echo $lastname;
+echo $firstname;
+echo $age;
 
-    ?>
+//   exo 3
+  echo $km . "<br>";
+
+  
+//   exo 4
+echo $text;
+echo $int . "<br>";
+echo $float . "<br>";
+
+// exo 5
+echo $int5 . "<br>";
+?>
+
+<!-- exo 6 -->
+<p>Bonjour <?php echo$exo6name ?> ,comment vas tu ? </p>
+
+
+<!-- exo  7 -->
+<p>Bonjour <?php echo $exo7lastname . " " . $exo7firstname; ?>, tu as <?php echo $exo7agename; ?> ans.</p>
+
+<!-- exo 8 -->
+
+<?php
+echo $exo8var1 . "<br>";
+echo $exo8var2 . "<br>";
+echo $exo8var3 . "<br>" ;
+
+?>
+
 </body>
 </html>
